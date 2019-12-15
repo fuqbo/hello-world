bcbac0b729ce01edfc7877d9900939398a1d6e20
fuqboo <31027042+fuqboo@users.noreply.github.com>
2019-12-15 23:12:56 +0800

    rr

---
+++ Stander_Cross_ARM_GCC_STM32F429iDISCO/Debug/Src/subdir.mk
@@ -0,0 +1,39 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/freertos.c \
+../Src/main.c \
+../Src/stm32f4xx_hal_msp.c \
+../Src/stm32f4xx_hal_timebase_TIM.c \
+../Src/stm32f4xx_it.c \
+../Src/system_stm32f4xx.c 
+
+OBJS += \
+./Src/freertos.o \
+./Src/main.o \
+./Src/stm32f4xx_hal_msp.o \
+./Src/stm32f4xx_hal_timebase_TIM.o \
+./Src/stm32f4xx_it.o \
+./Src/system_stm32f4xx.o 
+
+C_DEPS += \
+./Src/freertos.d \
+./Src/main.d \
+./Src/stm32f4xx_hal_msp.d \
+./Src/stm32f4xx_hal_timebase_TIM.d \
+./Src/stm32f4xx_it.d \
+./Src/system_stm32f4xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o: ../Src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM GNU C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=softfp -mfpu=fpv4-sp-d16 -O0 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -Wall  -g1 -D__weak="__attribute__((weak))" -D__packed="__attribute__((__packed__))" -DUSE_HAL_DRIVER -DSTM32F429xx -I../Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS -I../Drivers/CMSIS/Include -I../STM32F10x_FWLib/inc -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ Stander_Cross_ARM_GCC_STM32F429iDISCO/README.md
@@ -0,0 +1,4 @@
+这个工程主要根据一下网址中的教程配置，是使用arm eclipse插件配置而成的，在尝试使用时感觉还行，
+就是有时候不解析汇编文件，而在使用freeRTOS的时候还会崩溃
+
+https://mcuoneclipse.com/2013/07/21/diy-free-toolchain-for-kinetis-part-2-eclipse-ide/
